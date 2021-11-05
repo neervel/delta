07c950eff3f94a937c208d29f7f6671ebca5496c
Neervel <klihopoy@mail.ru>
2021-11-05 17:34:28 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(function() {
+  const closeVideo = $('.video-close')
+  const openVideo = $('#openVideo')
+  const videoPopup = $('.video')
+
+  openVideo.click(() => {
+    videoPopup.fadeIn()
+  })
+  closeVideo.click(() => {
+    videoPopup.fadeOut()
+    $('video')[0].pause()
+  })
+  videoPopup.click(() => {
+    if ($(event.target).hasClass('video-block')) {
+      videoPopup.fadeOut()
+      $('video')[0].pause()
+    }
+  })
+})
