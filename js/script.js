d3e094676eeae188ae8d88732309eea42e3d65f1
Neervel <klihopoy@mail.ru>
2021-11-07 21:39:53 +0300

    create slider & responsive

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
   new WOW().init()
-  
+
   const closeVideo = $('.video-close')
   const openVideo = $('#openVideo')
   const videoPopup = $('.video')
@@ -16,6 +16,17 @@ $(function() {
     if ($(event.target).hasClass('video-block')) {
       videoPopup.fadeOut()
       $('video')[0].pause()
+    }
+  })
+
+  $('.slick-slider').slick({
+    infinite: false,
+    dots: true,
+    nextArrow: '.next',
+    prevArrow: '.prev',
+    appendDots: '.slider-pagination',
+    customPaging: function (slick, index) {
+      return '<a>' + (index + 1) + '</a>';
     }
   })
 })
