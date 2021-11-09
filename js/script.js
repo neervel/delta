46ab5aca8dbe3cb62fb849ac78d4d574a10047e8
Neervel <klihopoy@mail.ru>
2021-11-09 14:01:44 +0300

    add animation

--- js/script.js
+++ js/script.js
@@ -32,7 +32,50 @@ $(function () {
     }
   })
 
-  $('#feedback').submit(function(e) {
+  $('#feedback').submit(function (e) {
     e.preventDefault();
   })
+
+  const html = document.documentElement;
+  const canvas = document.getElementById("animation");
+  const context = canvas.getContext("2d");
+
+  const frameCount = 150;
+  const currentFrame = index => (
+    `./assets/img/animation/${index.toString().padStart(3, '0')}.png`
+  )
+
+  const preloadImages = () => {
+    for (let i = 1; i < frameCount; i++) {
+      const img = new Image();
+        img.src = currentFrame(i);
+    }
+  };
+
+  const img = new Image()
+  img.src = currentFrame(1);
+  canvas.width = 1250;
+  canvas.height = 750;
+  img.onload = function () {
+    context.drawImage(img, 0, 0);
+  }
+
+  const updateImage = index => {
+    img.src = currentFrame(index);
+    context.drawImage(img, 0, 0);
+  }
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+      frameCount - 1,
+      Math.ceil(scrollFraction * frameCount)
+    );
+
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+  });
+
+  preloadImages()
 })
